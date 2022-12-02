24be148f317ede431fada6a4a1460e1e93c9027c
AMQIsmail <109195072+AMQIsmail@users.noreply.github.com>
2022-12-02 02:06:14 -0800

    Updates

---
+++ precache-manifest.9e010b0921077a7d549b83f08deb1ca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6372793f58e560f0ecc35e38d67f140",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a3e0fedff6e34d063b0a",
+    "url": "/test/static/css/main.372eaf11.chunk.css"
+  },
+  {
+    "revision": "7aebbbf77092e88f6814",
+    "url": "/test/static/js/2.58e1ea9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test/static/js/2.58e1ea9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e0fedff6e34d063b0a",
+    "url": "/test/static/js/main.6259f6f3.chunk.js"
+  },
+  {
+    "revision": "2a01cc75ac9178357fdd",
+    "url": "/test/static/js/runtime-main.d8f15352.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
