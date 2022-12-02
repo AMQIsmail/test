6d6f2603f456c20a7197e04fbef5655dccb6b082
AMQIsmail <109195072+AMQIsmail@users.noreply.github.com>
2022-12-02 02:20:53 -0800

    Updates

---
+++ precache-manifest.989f11680027fe23fa490131a429ac4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6571d3f0ececbb570c0b33c4a3c9ba82",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "06524d3f09085b0b176a",
+    "url": "/test/static/css/main.372eaf11.chunk.css"
+  },
+  {
+    "revision": "7aebbbf77092e88f6814",
+    "url": "/test/static/js/2.58e1ea9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test/static/js/2.58e1ea9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06524d3f09085b0b176a",
+    "url": "/test/static/js/main.b186fbb9.chunk.js"
+  },
+  {
+    "revision": "2a01cc75ac9178357fdd",
+    "url": "/test/static/js/runtime-main.d8f15352.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.883a3d94f3d7ae91a4c33029c2e25059.js"
+  "/test/precache-manifest.989f11680027fe23fa490131a429ac4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
